cc5044c846412872b64352558f17d2ac61d4ecf2
skrishna07 <srkrishna.mekala@bradsol.com>
2015-04-10 12:48:51 +0530

    commit 2

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -17,4 +17,4 @@ $this->pageTitle=Yii::app()->name;
 <p>For more details on how to further develop this application, please read
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+should you have any questions hai.</p>
